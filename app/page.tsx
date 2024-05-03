4994d3da1d40cd501cba9c72fdb943fc3cdbad71
iamtimleonard <timleonardcello@gmail.com>
2024-05-03 14:08:48 -0400

    fix: update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+
 function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <>
+      <h1>Hello, I am Tim Leonard 🚀</h1>
+      <a href="https://github.com/iamtimleonard">
+        <Image src="/github-mark.svg" alt="Github" width={50} height={50} />
+      </a>
+      <a href="https://www.linkedin.com/in/timothy-leonard-pa">
+        <Image src="/LI-Logo.png" alt="LinkedIn" width={50} height={50} />
+      </a>
+    </>);
 }
 
 export default Page;
