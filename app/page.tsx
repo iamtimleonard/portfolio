be30609474238197dce0a08e9adeb3b63d3354df
iamtimleonard <timleonardcello@gmail.com>
2024-09-06 15:57:34 -0400

    feat: basic grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,38 @@
 import Image from "next/image";
+import styles from "./home.module.css";
 
 function Page() {
   return (
-    <>
-      <h1>Hello, I am Tim Leonard 🚀</h1>
-      <a href="https://github.com/iamtimleonard">
-        <Image src="/github-mark.svg" alt="Github" width={50} height={50} />
-      </a>
-      <a href="https://www.linkedin.com/in/timothy-leonard-pa">
-        <Image src="/LI-Logo.png" alt="LinkedIn" width={50} height={50} />
-      </a>
-    </>);
+    <main className={styles.gridContainer}>
+      <div className={styles.nameCard}>
+        <h1>Hello, I am Tim Leonard 🚀</h1>
+      </div>
+      <div className={styles.skillCard}>
+        <ul>
+          <li>skill</li>
+          <li>skill</li>
+          <li>skill</li>
+        </ul>
+      </div>
+      <div className={styles.contactCard}>
+        <a href="https://github.com/iamtimleonard">
+          <Image src="/github-mark.svg" alt="Github" width={50} height={50} />
+        </a>
+        <a href="https://www.linkedin.com/in/timothy-leonard-pa">
+          <Image src="/LI-Logo.png" alt="LinkedIn" width={50} height={50} />
+        </a>
+      </div>
+      <div className={styles.bioCard}>
+        <p>
+          This is a biography of me. It could be boring. It could be
+          interesting. What else can I say?
+        </p>
+      </div>
+      <div className={styles.miscCard}>
+        <p>gonna be other stuff here</p>
+      </div>
+    </main>
+  );
 }
 
 export default Page;
