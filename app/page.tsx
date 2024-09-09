15d80d711b0493bda264ba8a0235e28163f610c3
iamtimleonard <timleonardcello@gmail.com>
2024-09-09 10:27:52 -0400

    feat(grid): styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,17 @@ import styles from "./home.module.css";
 function Page() {
   return (
     <main className={styles.gridContainer}>
-      <div className={styles.nameCard}>
+      <div className={`${styles.nameCard} ${styles.card}`}>
         <h1>Hello, I am Tim Leonard 🚀</h1>
       </div>
-      <div className={styles.skillCard}>
+      <div className={`${styles.skillCard} ${styles.card}`}>
         <ul>
           <li>skill</li>
           <li>skill</li>
           <li>skill</li>
         </ul>
       </div>
-      <div className={styles.contactCard}>
+      <div className={`${styles.contactCard} ${styles.card}`}>
         <a href="https://github.com/iamtimleonard">
           <Image src="/github-mark.svg" alt="Github" width={50} height={50} />
         </a>
@@ -22,13 +22,13 @@ function Page() {
           <Image src="/LI-Logo.png" alt="LinkedIn" width={50} height={50} />
         </a>
       </div>
-      <div className={styles.bioCard}>
+      <div className={`${styles.bioCard} ${styles.card}`}>
         <p>
           This is a biography of me. It could be boring. It could be
           interesting. What else can I say?
         </p>
       </div>
-      <div className={styles.miscCard}>
+      <div className={`${styles.miscCard} ${styles.card}`}>
         <p>gonna be other stuff here</p>
       </div>
     </main>
