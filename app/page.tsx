92c8f6f5e440504ac5ecae417eb71acc9832f7c7
iamtimleonard <timleonardcello@gmail.com>
2024-09-09 10:48:56 -0400

    feat(home): update content

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,13 @@ function Page() {
       </div>
       <div className={`${styles.skillCard} ${styles.card}`}>
         <ul>
-          <li>skill</li>
-          <li>skill</li>
-          <li>skill</li>
+          <li>Node.js</li>
+          <li>AWS</li>
+          <li>MongoDB</li>
+          <li>Redis</li>
+          <li>React</li>
+          <li>Ember</li>
+          <li>Svelte</li>
         </ul>
       </div>
       <div className={`${styles.contactCard} ${styles.card}`}>
@@ -24,12 +28,35 @@ function Page() {
       </div>
       <div className={`${styles.bioCard} ${styles.card}`}>
         <p>
-          This is a biography of me. It could be boring. It could be
-          interesting. What else can I say?
+          I am a web developer with more than three years of professional
+          experience, specializing in building dynamic, scalable web
+          applications.
+        </p>
+        <p>
+          I first started working at 21PSTEM, an education-focused non-profit in
+          the Philadelphia area, where I coordinated with math educators to
+          develop technologies to foster student colaboration in the learning
+          process. Currently, I am part of the team at epoxy.ai, where I help
+          develop cutting-edge technology in the sports betting space.
+        </p>
+        <p>
+          With a passion for problem-solving and a focus on delivering
+          efficient, user-friendly applications, I thrive in dynamic
+          environments that encourage growth and learning.
         </p>
       </div>
       <div className={`${styles.miscCard} ${styles.card}`}>
-        <p>gonna be other stuff here</p>
+        <figure>
+          <figcaption>I have other interests like:</figcaption>
+          <ul>
+            <li>mycology</li>
+            <li>rock climbing</li>
+            <li>shape note singing</li>
+            <li>sewing</li>
+            <li>fermentation</li>
+            <li>food</li>
+          </ul>
+        </figure>
       </div>
     </main>
   );
