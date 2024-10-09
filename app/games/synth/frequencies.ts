3f3a4449ea516eb6cf375bf44710ba6e818dfd68
iamtimleonard <timleonardcello@gmail.com>
2024-10-08 23:43:30 -0400

    feat(games): first pass at synth

---
+++ app/games/synth/frequencies.ts
@@ -0,0 +1,15 @@
+export default {
+  a: { frequency: 523.2511, type: "white", key: "a", note: "c" }, //c
+  w: { frequency: 554.3653, type: "black", key: "w", note: "c#" }, //c#
+  s: { frequency: 587.3295, type: "white", key: "s", note: "d" }, //d
+  e: { frequency: 622.254, type: "black", key: "e", note: "d#" }, //d#
+  d: { frequency: 659.2551, type: "white", key: "d", note: "e" }, //e
+  f: { frequency: 698.4565, type: "white", key: "f", note: "f" }, //f
+  t: { frequency: 739.9888, type: "black", key: "t", note: "f#" }, //f#
+  g: { frequency: 783.9099, type: "white", key: "g", note: "g" }, //g
+  y: { frequency: 830.6094, type: "black", key: "y", note: "g#" }, //g#
+  h: { frequency: 880, type: "white", key: "h", note: "a" }, //a
+  u: { frequency: 932.3275, type: "black", key: "u", note: "a#" }, //a#
+  j: { frequency: 987.7666, type: "white", key: "j", note: "b" }, //b
+  k: { frequency: 1046.502, type: "white", key: "k", note: "c" }, //c
+};
--- app/games/synth/page.tsx
+++ app/games/synth/page.tsx
@@ -1,3 +1,75 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import frequencies from "./frequencies";
+import styles from "./synth.module.css";
+
+const audioContext = new AudioContext();
+const oscillator = new OscillatorNode(audioContext);
+const gain = new GainNode(audioContext, {
+  gain: 0,
+});
+oscillator.connect(gain);
+gain.connect(audioContext.destination);
+
+let timeout = setTimeout(() => {}, 0);
+
 export default function Page() {
-  return <h1>SYTHESIZER!!!</h1>;
+  const [synthType, setSynthType] = useState<OscillatorType>("sine");
+  const [started, setStarted] = useState<boolean>(false);
+  const [pressed, setPressed] = useState<string>("");
+  const handleKeyboardInput = useCallback((e: KeyboardEvent) => {
+    if (frequencies[e.key]) {
+      if (timeout) clearTimeout(timeout);
+      oscillator.frequency.value = frequencies[e.key].frequency;
+      gain.gain.linearRampToValueAtTime(0.75, 0.5);
+      timeout = setTimeout(() => {
+        gain.gain.linearRampToValueAtTime(0, 1);
+      }, 1000);
+      setPressed(e.key);
+      setTimeout(() => setPressed(""), 1000);
+    }
+    return;
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => handleKeyboardInput(e), false);
+  }, []);
+  return (
+    <div className={styles.container}>
+      <h1>SYTHESIZER!!!</h1>
+      {!started && (
+        <button
+          onClick={() => {
+            oscillator.start();
+            setStarted(true);
+          }}
+        >
+          start
+        </button>
+      )}
+      <select
+        name="wave"
+        onChange={(e) => {
+          oscillator.type = e.target.value as OscillatorType;
+          setSynthType(e.target.value as OscillatorType);
+        }}
+      >
+        <option value="sine">Sine</option>
+        <option value="square">Square</option>
+        <option value="sawtooth">Sawtooth</option>
+        <option value="triangle">Triangle</option>
+      </select>
+      <div className={styles.keyboard}>
+        {Object.values(frequencies).map(({ key, frequency, note, type }) => (
+          <div
+            className={`${styles[type]} ${pressed === key && styles.pressed}`}
+            key={frequency}
+          >
+            {note}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
