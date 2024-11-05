386f4596c90e9ea0e3f30cadbee3fdcb68c2dcd9
iamtimleonard <timleonardcello@gmail.com>
2024-11-04 21:54:47 -0500

    feat(main): redesign home

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,37 @@
+import Link from "next/link";
 import "./global.css";
+import styles from "./home.module.css";
 
 export const metadata = {
   title: "Tim Leonard's Personal Website",
   description: "iamtimleonard",
 };
+
+function NavItem({ href, title }) {
+  return (
+    <li className={styles.navListItem}>
+      <Link href={href} className={styles.navLink}>
+        {title}
+      </Link>
+    </li>
+  );
+}
+
+function NavBar() {
+  return (
+    <div className={styles.navContainer}>
+      <header className={styles.navBar}>
+        <nav className={styles.nav}>
+          <ol className={styles.navList}>
+            <NavItem href="/" title="Home" />
+            <NavItem href="/about" title="About Me" />
+            <NavItem href="/things-i-use" title="Things I Use" />
+          </ol>
+        </nav>
+      </header>
+    </div>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -12,7 +40,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={styles.body}>
+        <NavBar></NavBar>
+        <div className={styles.container}>{children}</div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,129 +1,30 @@
-"use client";
-
-import styles from "./home.module.css";
-import { DiMongodb } from "react-icons/di";
-import {
-  FaAws,
-  FaEmber,
-  FaGithub,
-  FaLinkedin,
-  FaNodeJs,
-  FaReact,
-} from "react-icons/fa6";
-import { SiGit, SiRedis, SiSvelte } from "react-icons/si";
-import { IconContext } from "react-icons";
 import Link from "next/link";
-import Image from "next/image";
-import encompassLogo from "../public/logo-encompass.gif";
-import epoxyLogo from "../public/epoxy.jpeg";
-import { Fade } from "react-awesome-reveal";
-import TypeIt from "typeit-react";
-
-function Card({ styleReference, children }) {
-  return (
-    <div className={styles.experiment}>
-      <div className={styles.cardContainer}>
-        <IconContext.Provider value={{ className: styles.reactIcons }}>
-          <Fade>
-            <div className={`${styles.card} ${styleReference}`}>{children}</div>
-          </Fade>
-        </IconContext.Provider>
-      </div>
-    </div>
-  );
-}
+import styles from "./home.module.css";
 
 function Page() {
   return (
-    <div className={styles.background}>
-      <main className={styles.mainContainer}>
-        <Card styleReference={styles.nameCard}>
-          <article>
-            <TypeIt
-              as={"h1"}
-              options={{
-                cursor: false,
-              }}
+    <div className={styles.asymmetric}>
+      <main className={styles.mainContent}>
+        <div className={styles.container}>
+          <h1 className={styles.neon}>Hello, I am Tim Leonard</h1>
+          <p className={styles.info}>
+            Find me on{" "}
+            <Link
+              href="https://github.com/iamtimleonard"
+              className={styles.outsideLink}
             >
-              Hello, I am Tim Leonard 🚀
-            </TypeIt>
-          </article>
-          <footer>
-            <a href="https://github.com/iamtimleonard">
-              <FaGithub />
-            </a>
-            <a href="https://www.linkedin.com/in/timothy-leonard-pa">
-              <FaLinkedin />
-            </a>
-          </footer>
-        </Card>
-        <Card styleReference={styles.bioCard}>
-          <article id="about-me">
-            <p>
-              I began making websites for fun with friends in elementary school,
-              when table-based layouts were all the rage. Somewhere in the
-              depths of the internet there is an archive of the geocities site I
-              made to review video games. Currently, I am a full stack engineer
-              at Epoxy.ai, where I collaborate with a team of experienced
-              engineers to create personalization tools in the sports betting
-              space. While there, I gained experience designing and building
-              scalable web applications with AWS. I have been enjoying getting
-              my hands dirty with cloud native architecture. Previously, I
-              worked at 21PSTEM, a non-profit specializing in education
-              technology for STEM classrooms. There, I worked directly with
-              educators and researches to build applications for use in math
-              classes. That work tied in well with my own history as an educator
-              and public school teacher.
-            </p>
-            <p>
-              I began my journey as a self-taught web developer. To hone my
-              skills, I took part in projects with chingu.io, where I was
-              grouped with other web developers across the world to build
-              projects together. I got to work on some interesting projects with
-              equally interesting people - a standout was a mushroom locator
-              app. It taught me a lot about software design and working as a
-              team of developers.
-            </p>
-            <p>
-              In an earlier life, I was a professional cellist, freelancing in
-              New York City, and a public school music teacher in Warren
-              Township, NJ, where I taught instrumental music.
-            </p>
-            <p>
-              I am also an avid rock climber, shape note singer, mycologist, and
-              Seinfeld fan.
-            </p>
-            <figure>
-              <figcaption>Things I use: </figcaption>
-              <ul>
-                <li>
-                  <FaNodeJs title="Node.js" />
-                </li>
-                <li>
-                  <FaAws title="AWS" />
-                </li>
-                <li>
-                  <DiMongodb title="MongoDB" />
-                </li>
-                <li>
-                  <SiRedis title="Redis" />
-                </li>
-                <li>
-                  <FaReact title="React" />
-                </li>
-                <li>
-                  <FaEmber title="Ember" />
-                </li>
-                <li>
-                  <SiSvelte title="Svelte" />
-                </li>
-                <li>
-                  <SiGit title="Git" />
-                </li>
-              </ul>
-            </figure>
-          </article>
-        </Card>
+              GitHub
+            </Link>{" "}
+            and{" "}
+            <Link
+              href="https://www.linkedin.com/in/timothy-leonard-pa/"
+              className={styles.outsideLink}
+            >
+              LinkedIn
+            </Link>
+            .
+          </p>
+        </div>
       </main>
     </div>
   );
