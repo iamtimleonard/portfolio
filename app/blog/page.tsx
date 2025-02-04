710efd72dfffbb0b62850ae8770b7e31a026743b
iamtimleonard <timleonardcello@gmail.com>
2025-02-03 22:37:00 -0500

    feat(blog): add blog post

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,11 +3,11 @@ import entries from "../../lib/blog.json"
 
 const Page = () => (
   <div className={styles.blog}>
-    {entries.map((entry) => (
-      <article className={styles.entry}>
+    {entries.reverse().map((entry) => (
+      <article key={entry.id} className={styles.entry}>
         <p className={styles.date}>{new Date(entry.date).toLocaleDateString()}</p>
         <h3 className={styles.title}>{entry.title}</h3>
-        <p className={styles.text}>{entry.text}</p>
+        <div dangerouslySetInnerHTML={{ __html: entry.text}} />
       </article>
     ))}
   </div>
